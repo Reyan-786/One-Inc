a7cef2a7cb20e348f0fb2a9d6ddcaef389d4469b
Mohd Rehan <reyanabidin169@gmail.com>
2023-09-29 16:49:50 +0530

    One commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import pic from './hero-section-image.svg';
 import rehanpic from './Rehan.jpeg'
 import faisalpic from './Faisalpic.jpeg'
+import adnaanpic from './Adnaanpic.jpeg';
 function App() {
   return (
     <div className="App">
@@ -83,31 +84,34 @@ function App() {
       <section className="team-section">
           <h2>Team</h2>
           <p>Our Team is highly skilled in Software Development, Data Analytics, Machine Learning & A.I.</p>
+          <div className='Team-Container'>
           <div className="team-member">
           <div className="member-content">
             <img className="team-img" src={rehanpic} alt='Mohd Rehan' />
             <div className="member-info">
-              <h3>Mohd Rehan</h3>
-              <p>
-              <a href='#' target="_blank" rel="noopener noreferrer">
+              <p className='Member-Name'>Mohd Rehan</p>
               <a href="https://www.linkedin.com/" target="_blank" class="social-icon"><i class="fab fa-linkedin"></i></a>
-              </a>
-              </p>
               </div>
             </div>
           </div>
           <div className="team-member">
           <div className="member-content">
-            <img className="team-img" src={faisalpic} alt='Mohd Rehan' />
+            <img className="team-img" src={faisalpic} alt='Faisal Khan' />
             <div className="member-info">
-              <h3>Faisal Ali Khan</h3>
-              <p>
-              <a href='#' target="_blank" rel="noopener noreferrer">
-              <a href="https://www.linkedin.com/" target="_blank" class="social-icon"><i class="fab fa-linkedin"></i></a>
-              </a>
-              </p>
+            <p className = 'Member-Name'>Faisal Khan</p>
+            <a href="https://www.linkedin.com/" target="_blank" class="social-icon"><i class="fab fa-linkedin"></i></a>
               </div>
             </div>
+          </div>
+          <div className="team-member">
+          <div className="member-content">
+            <img className="team-img" src={adnaanpic} alt='Mohd Adnaan' />
+            <div className="member-info">
+            <p className = 'Member-Name'>Mohd Adnaan</p>
+            <a href="https://www.linkedin.com/" target="_blank" class="social-icon"><i class="fab fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
           </div>
       </section>
 
